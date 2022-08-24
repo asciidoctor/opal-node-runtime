68e6ebfa9a09a559581bb9a818c6210061f10702
Guillaume Grossetie <ggrossetie@gmail.com>
2022-08-24 15:55:13 +0200

    resolves #134 publish a browser-compatible entry point

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,31 @@
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'src/index.cjs',
-    format: 'cjs',
+import replace from 'rollup-plugin-re'
+
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'src/index.cjs',
+      format: 'cjs',
+    },
+    external: ['fs', 'glob', 'os', 'path', 'unxhr', 'util']
   },
-  external: [ 'fs', 'glob', 'os', 'path', 'unxhr', 'util' ]
-}
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'src/index.mjs',
+      format: 'es',
+    },
+    plugins: [
+      replace({
+        patterns: [
+          {
+            // string or regexp
+            test: 'import \'./nodejs.js\'',
+            // string or function to replaced with
+            replace: '',
+          }
+        ]
+      }),
+    ]
+  }
+]
